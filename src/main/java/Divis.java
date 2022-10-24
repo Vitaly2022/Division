20ebf818798b671b96f05f63a88cbfa88256c731
VITALY <9004673@mail.ru>
2022-10-24 22:10:42 +0300

    Initial commit

---
+++ src/main/java/Divis.java
@@ -0,0 +1,60 @@
+public class Divis {
+
+    int delim;
+    int delitel;
+    String rez = "";
+    int lNum;
+
+
+    public Divis(int delim, int delitel, int lNum) {
+        this.delim = delim;
+        this.delitel = delitel;
+        this.lNum = lNum;
+    }
+      //DELIM 3000
+      //DELITEL 500
+
+    public int count() { //находим количество целых вхождений
+        int iC = 1;
+        int CountDelim =delim;
+        while (CountDelim >= delitel) {
+            CountDelim = CountDelim - delitel;
+            if (CountDelim >= delitel) iC++;
+        }
+        return iC;
+    }
+
+    public int uvelich() { //Если делимое меньше - увеличиваем
+        int r=0;
+        while (delim < delitel) {
+            r++;
+            delim = delim * 10;
+            if (r>=2) rez = rez + "0";//тута косяк - лишний ноль
+        }
+        return delim;
+    }
+
+    public String result() {
+        int i=0;// индикатор количества знаков после запятой
+        int razn=-1;
+        if (delim<delitel) rez="0";
+        while (i != lNum-1) {
+             if (delitel <= delim) {
+             rez=rez+String.valueOf(count());
+             delim=delim-count()*delitel;
+             if (delim==0) i=lNum-1; //выходим из цикла если поделились без остатка
+
+            } else {      //если делитель больше
+             i++;
+             if (i==1) rez = rez + ",";
+             delim=uvelich();
+             rez=rez+String.valueOf(count());
+             delim=delim-count()*delitel;
+             if (delim==0) i=lNum-1; //выходим из цикла если поделились без остатка
+            }
+
+        }
+        return rez;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    Divis d = new Divis(20000,1023,7);
+    String s;
+    s=d.result();
+    System.out.println("REZ="+s);
+
+    }
+}
